3dc86f6bbd205c10c08ae866215fd5227f6b942f
vivekgupta999 <vivekkumargupta2011@gmail.com>
2022-02-11 00:27:28 +0400

    commit changes

--- Sources/RandomNumber/RandomNumber.swift
+++ Sources/RandomNumber/RandomNumber.swift
@@ -2,7 +2,12 @@ import SwiftUI
  
 @available(iOS 15.0, macOS 12.0, *)
 public struct RandomNumber: View {
-    @ObservedObject var random : RandomNumberVM
+    @Binding var random : RandomNumberVM
+    
+    public init(random: Binding<RandomNumberVM>){
+        self._random = random
+    }
+    
     public var body: some View {
         VStack{
             TextField("Enter Number", value: $random.number, formatter: NumberFormatter())
